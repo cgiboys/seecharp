509019048c84a36585bcaf8e0756807b894dcdee
Bjorn <bjorn.blomberg@cgi.com>
2023-06-09 12:02:07 +0000

    add unittest for c# .net

--- web.cs
+++ web.cs
@@ -7,39 +7,43 @@ namespace WebServer
 {
     class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
+    {
+        string folderPath = @"www/"; // Change this to the path of the folder containing the files you want to host
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:8080/"); // Change this to the desired port number
+        listener.Start();
+        Console.WriteLine("Web server running...");
+        while (true)
         {
-            string folderPath = @"www/"; // Change this to the path of the folder containing the files you want to host
-            HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost:8080/"); // Change this to the desired port number
-            listener.Start();
-            Console.WriteLine("Web server running...");
-            while (true)
+            HttpListenerContext context = listener.GetContext();
+            ThreadPool.QueueUserWorkItem((o) =>
             {
-                HttpListenerContext context = listener.GetContext();
-                ThreadPool.QueueUserWorkItem((o) =>
+                HttpListenerResponse response = context.Response;
+                string filePath = context.Request.Url.AbsolutePath;
+                filePath = filePath.Substring(1);
+                if (filePath == "")
                 {
-                    HttpListenerResponse response = context.Response;
-                    string filePath = context.Request.Url.AbsolutePath;
-                    filePath = filePath.Substring(1);
-                    filePath = Path.Combine(folderPath, filePath);
-                    if (File.Exists(filePath))
-                    {
-                        byte[] buffer = File.ReadAllBytes(filePath);
-                        response.ContentLength64 = buffer.Length;
-                        response.OutputStream.Write(buffer, 0, buffer.Length);
-                        response.OutputStream.Close();
-                    }
-                    else
-                    {
-                        string error = "File not found";
-                        byte[] errorBuffer = System.Text.Encoding.UTF8.GetBytes(error);
-                        response.ContentLength64 = errorBuffer.Length;
-                        response.OutputStream.Write(errorBuffer, 0, errorBuffer.Length);
-                        response.OutputStream.Close();
-                    }
-                }, null);
-            }
+                    filePath = "index.html";
+                }
+                filePath = Path.Combine(folderPath, filePath);
+                if (File.Exists(filePath))
+                {
+                    byte[] buffer = File.ReadAllBytes(filePath);
+                    response.ContentLength64 = buffer.Length;
+                    response.OutputStream.Write(buffer, 0, buffer.Length);
+                    response.OutputStream.Close();
+                }
+                else
+                {
+                    string error = "File not found";
+                    byte[] errorBuffer = System.Text.Encoding.UTF8.GetBytes(error);
+                    response.ContentLength64 = errorBuffer.Length;
+                    response.OutputStream.Write(errorBuffer, 0, errorBuffer.Length);
+                    response.OutputStream.Close();
+                }
+            }, null);
         }
+    }
     }
 }
